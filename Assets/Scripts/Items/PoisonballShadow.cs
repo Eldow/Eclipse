80c2701ba8bd90a74e37760e650e9a7b1ded4abe
Eldow <francois.decan@free.fr>
2016-12-09 17:24:10 -0500

    added files

---
+++ Assets/Scripts/Items/PoisonballShadow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoisonballShadow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.Equals(Switcher.instance.currentPlayer))
+        {
+            Switcher.instance.KillPlayer(Switcher.instance.currentPlayer);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenario/Intro.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+    public Sprite profHead;
+    public Sprite raHead;
+    public Sprite apoHead;
+	// Use this for initialization
+	void Awake () {
+        StartCoroutine(NextLevel());
+	}
+	IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(5f);
+        TextLogger.instance.SetSpriteAndText(profHead, "What is this shine ?!");
+        yield return new WaitForSeconds(5f);
+        TextLogger.instance.SetSpriteAndText(profHead, "It looks like the legendary solar disk !");
+        yield return new WaitForSeconds(5f);
+        GameObject.Find("Ra").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("RaShadow").GetComponent<SpriteRenderer>().enabled = true;
+        TextLogger.instance.SetSpriteAndText(raHead, "How dare you lay your hands on the symbol of my power ?!");
+        yield return new WaitForSeconds(5f);
+        TextLogger.instance.SetSpriteAndText(raHead, "By the sunlight I curse you !");
+        GameObject.Find("Torch").GetComponent<Torch>().Activate();
+        yield return new WaitForSeconds(5f);
+        GameObject.Find("Apophis").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("ApophisShadow").GetComponent<SpriteRenderer>().enabled = true;
+        TextLogger.instance.SetSpriteAndText(apoHead, "Hahahahaha ! Thank you foreigner for helping me finding this one !");
+        yield return new WaitForSeconds(5f);
+        TextLogger.instance.SetSpriteAndText(raHead, "Apophis ?! No ! WAKE UP DUDE ! Don't let him steal my ...  !?");
+        yield return new WaitForSeconds(5f);
+        GameObject.Find("Apophis").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("ApophisShadow").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("SolarDisk").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("SolarDiskShadow").GetComponent<SpriteRenderer>().enabled = false;
+        TextLogger.instance.SetSpriteAndText(raHead, "Oh, screw me.");
+        yield return new WaitForSeconds(5f);
+        Stage.instance.NextStage();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
