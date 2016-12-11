162899cb21d10832d1a63f79794d9fc79978ae74
Eldow <francois.decan@free.fr>
2016-12-10 20:49:01 -0500

    fix stage2 & credits

---
+++ Assets/Scripts/Scenario/TextSlider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class TextSlider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ToMainMenu());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetChild(0).position = new Vector2(transform.GetChild(0).position.x, transform.GetChild(0).position.y + 1f);
+	}
+
+    IEnumerator ToMainMenu()
+    {
+        yield return new WaitForSeconds(55f);
+        SceneManager.LoadScene(0);
+    }
+}
